facd076e72d9b31f77419c7bef13f1dae5ccaffe
Alexandre Castro <im-alexandre07@gmail.com>
2021-10-28 13:43:18 -0300

    sobrescrevendo atributos

--- metatables.lua
+++ metatables.lua
@@ -1,13 +1,13 @@
 fallback = {
+    life = 100,
     mana = 50
 }
 
 object = {
-    life = 100
+    life = 80
 }
 
 setmetatable(object, {__index = fallback})
 
 print(object.life)
 print(object.mana)
-
