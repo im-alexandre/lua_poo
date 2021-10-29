5c2b04ad71264f70d62fc90e82071afd132e7b58
Alexandre Castro <im-alexandre07@gmail.com>
2021-10-29 10:49:00 -0300

    Utilizando herança com metatables
    Para definir os métodos fora da instância em `knight`, definimos a
    metatable da instância como `Knight` e definimos a metatable de `Knight`
    como `Hero`.
    Se não definirmos a metatable de `Knight` como `Hero`, a instância vai
    procurar os métodos nela mesma e não vai encontrar.
    Caso a metatable de `Knight` seja definida como `Hero`, o interpretador vai
    buscar os métodos da instância, do `Knight` e do `Hero`

--- knight.lua
+++ knight.lua
@@ -1,11 +1,17 @@
 local Hero = require('hero')
 
 local Knight = {}
+setmetatable(Knight, {__index = Hero})
 
 function Knight:new(attributes)
     local instance = Hero:new()
-
+    instance._damage = instance.damage
+    setmetatable(instance, {__index = self})
     return instance
+end
+
+function Knight:damage(value)
+    self:_damage(value * 0.5)
 end
 
 return Knight
