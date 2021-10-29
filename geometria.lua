38b9eb1042aa57d5a17ee3ed2ef42171496f57cc
Alexandre Castro <im-alexandre07@gmail.com>
2021-10-29 11:41:02 -0300

    Utilizando memoization (cálculos demorados)
    Utilizando um cache para armazenar resultados de funções pesadas ou
    demoradas

--- geometria.lua
+++ geometria.lua
@@ -2,6 +2,8 @@ local geometria = {}
 
 local constants = {}
 
+local cache = {}
+
 setmetatable(
     constants,
     {
@@ -12,7 +14,8 @@ setmetatable(
     }
     )
 
-constants.PI = 5
+--Alterar a constante levanta um erro
+--constants.PI = 5
 
 -- Métodos estáticos
 function geometria.area(altura, largura)
@@ -21,6 +24,26 @@ end
 
 function geometria.circle_area(radius)
     return constants.PI * math.pow(radius, 2)
+end
+
+function geometria.heavy()
+    local key = 'heavy'
+    if cache[key] then
+        return cache[key]
+    end
+
+    sleep(2)
+
+    result = 10
+    cache[key] = result
+    
+    return result
+
+end
+
+function sleep(valor)
+    local tempo = tonumber(os.clock() + valor)
+    while(os.clock() < tempo) do end
 end
 
 return geometria
